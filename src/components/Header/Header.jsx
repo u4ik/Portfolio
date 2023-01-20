9904771a5ebbed4cc36ef88ea4166d94f0ddf2ab
u4ik <insighteuphoric@gmail.com>
2023-01-20 07:43:44 -0500

    fade in

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,33 +8,17 @@ import { AnimationOnScroll } from 'react-animation-on-scroll';
 import { Badge } from '../../components'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-
+import { motion } from "framer-motion";
 
 import './Header.css'
 
 const Header = () => {
     const [showBackground, setShowBackground] = useState(false);
-    const [showName, setShowName] = useState(false);
-    const [showTitle, setShowTitle] = useState(false);
+
     const [showCloud, setShowCloud] = useState(false);
 
-    const [showAboutSkills, setShowAboutSkills] = useState(false);
-    const [isVisible, setIsVisible] = useState(false);
-    const [calculations, setCalculations] = useState({
-        direction: 'none',
-        height: 0,
-        width: 0,
-        topPassed: false,
-        bottomPassed: false,
-        pixelsPassed: 0,
-        percentagePassed: 0,
-        topVisible: false,
-        bottomVisible: false,
-        fits: false,
-        passing: false,
-        onScreen: false,
-        offScreen: false,
-    });
+
+
     const ref = useRef(null);
     useEffect(() => {
         AOS.init({ duration: 2000 })
@@ -44,12 +28,8 @@ const Header = () => {
         setTimeout(() => {
             setShowCloud(true)
         }, 500)
-        setTimeout(() => {
-            setShowName(true)
-        }, 700)
-        setTimeout(() => {
-            setShowTitle(true)
-        }, 1000)
+
+
     }, [])
     const handleUpdate = (e, { calculations }) => {
         setCalculations({ calculations })
@@ -78,49 +58,78 @@ const Header = () => {
                         <img src={Cloud} alt="" className='foreground' />
                     </CSSTransition>
                     {/* NAME + TITLE */}
-                    <div className="name-title_wrapper">
+                    <motion.div
+                        whileInView={{ opacity: 1 }}
+                        initial={{ opacity: 0 }}
+                        transition={{
+                            duration: 0.25,
+                            delay: 0.5
+                        }}
+                    >
+                        <div className="name-title_wrapper">
 
-                        <h1 className="name" style={{ marginInline: '.3em' }} data-aos="fade" data-aos-easing='ease-in-out' data-aos-delay="50" data-aos-once="false">Amit Mangat</h1>
-
-
-                        <p className="title" style={{ margin: '0' }} data-aos="fade" data-aos-easing='ease-in-out' data-aos-delay="500" data-aos-once="false">Web Developer</p>
-
-                    </div>
-                </header >
-
-                <div className="about_skills-container"   >
-                    <div className="about-wrapper" >
-                        <h1 className="about-headings" >About</h1>
-                        <p className="about-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia laudantium, ex facilis culpa nihil doloribus voluptatem architecto et? Unde dolor, aliquid iste facere veniam reiciendis dolorem perferendis vero. Rerum numquam voluptatibus possimus incidunt, placeat
-                        </p>
-                    </div>
-                    <Divider >_______________________________________</Divider>
+                            <h1 className="name" style={{ marginInline: '.3em' }} data-aos="fade" data-aos-easing='ease-in-out' data-aos-delay="50" data-aos-once="false">Amit Mangat</h1>
 
 
+                            <p className="title" style={{ margin: '0' }} data-aos="fade" data-aos-easing='ease-in-out'
+                                data-aos-delay="500" data-aos-once="false">Web Developer</p>
 
-                    <div className="skills-wrapper" >
-                        <h1 className="about-headings" >Skills</h1>
-
-                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', flexWrap: '', gap: '.5em' }}>
-                            <div className='col'>
-                                <Badge color="green" label="Node" />
-                                <Badge color="white" label="JavaScript" />
-                                <Badge color="blue" label="TypeScript" />
-                                <Badge color="olive" label="C#" />
-                                <Badge color="yellow" label="Python" />
-                            </div>
-                            <div className='col'>
-                                <Badge color="green" label="Node" />
-                                <Badge color="white" label="JavaScript" />
-                                <Badge color="blue" label="TypeScript" />
-                                <Badge color="olive" label="C#" />
-                                <Badge color="yellow" label="Python" />
-                            </div>
                         </div>
+                    </motion.div>
+                </header >
+                <div className="about_skills-overlay">
+                    <div className="about_skills-container"   >
+                        <div className="about-wrapper">
+                            <motion.div
+                                whileInView={{ opacity: 1 }}
+                                initial={{ opacity: 0 }}
+                                transition={{
+                                    duration: 0.25,
+                                    delay: 0.5
+                                }}
+                            >
+                                <h1 className="about-headings" >About</h1>
+                                <p className="about-text">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia laudantium, ex facilis culpa nihil doloribus voluptatem architecto et? Unde dolor, aliquid iste facere veniam reiciendis dolorem perferendis vero. Rerum numquam voluptatibus possimus incidunt, placeat
+                                </p>
+                            </motion.div>
+                        </div>
+                        <Divider >_______________________________________</Divider>
+                        <div className="skills-wrapper"
+                        //  data-aos="fade" 
+                        // data-aos-easing='ease-in-out'
+                        //  data-aos-offset="-490"
+                        >
+                            <motion.div
+                                whileInView={{ opacity: 1 }}
+                                initial={{ opacity: 0 }}
+                                transition={{
+                                    duration: 0.25,
+                                    delay: 0.5
+                                }}
+                            >
+                                <h1 className="about-headings" >Skills</h1>
+
+                                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', flexWrap: '', gap: '.5em' }}>
+                                    <div className='col'>
+                                        <Badge color="green" label="Node" />
+                                        <Badge color="white" label="JavaScript" />
+                                        <Badge color="blue" label="TypeScript" />
+                                        <Badge color="olive" label="C#" />
+                                        <Badge color="yellow" label="Python" />
+                                    </div>
+                                    <div className='col'>
+                                        <Badge color="green" label="Node" />
+                                        <Badge color="white" label="JavaScript" />
+                                        <Badge color="blue" label="TypeScript" />
+                                        <Badge color="olive" label="C#" />
+                                        <Badge color="yellow" label="Python" />
+                                    </div>
+                                </div>
+                            </motion.div>
+                        </div>
+
                     </div>
-
-
                 </div>
 
 
