8da752326075caaddf9b0ce170d9649c656aeef8
u4ik <insighteuphoric@gmail.com>
2023-02-14 00:20:29 -0500

    adjust skills bottom margin

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,35 +12,36 @@ const Navbar = () => {
     }
 
     return (
-        <nav>
-            <Popup
-                on='click'
-                inverted
-                hideOnScroll
-                trigger={
-                    <div style={{ width: '100%', backgroundColor: 'red', fontWeight: '300', paddingBottom: '', cursor: 'pointer', fontFamily: "Inter, Avenir, Helvetica, Arial, sans-serif" }}><b><p>
-                        Site is currently under construction 🛠️ (Click for more info!)
-                    </p>
-                    </b>
-                    </div>}
-                style={style}
-                content={'Currently importing my projects and redeploying 🛠️... \n Certain parts of the my site may not be available. \n Projects are currently being prioritized.'}
-                size='mini'
-                position='bottom center'
-                wide />
-            <ul>
-                <li>
-                    <Link to="/"><i className={`icon home blue small`} style={{ marginRight: '' }}></i>Home</Link>
-                </li>
-                <li>
-                    <Link to="/contact"><i className={`icon envelope orange small`} style={{ scale: '', marginRight: '' }}></i>Contact</Link>
-                </li>
-                <li>
-                    <Link to="/projects"><i className={`icon folder open brown small`} style={{ scale: '', marginRight: '' }}></i>Projects</Link>
-                </li>
-                <li>
-                    <Link to="/resume"><i className={`icon download green small`} style={{ scale: '', marginRight: '' }}></i>Resume </Link>
-                    {/* <Popup
+        <>
+            <nav>
+                <Popup
+                    on='click'
+                    inverted
+                    hideOnScroll
+                    trigger={
+                        <div style={{ width: '100%', backgroundColor: 'red', borderRadius: '0 0 1em 1em', fontWeight: '300', paddingBottom: '', cursor: 'pointer', fontFamily: "Inter, Avenir, Helvetica, Arial, sans-serif" }}><b><p>
+                            Site is currently under construction 🛠️ (Click for more info!)
+                        </p>
+                        </b>
+                        </div>}
+                    style={style}
+                    content={'Currently importing my projects and redeploying 🛠️... \n Certain parts of the my site may not be available. \n Projects are currently being prioritized.'}
+                    size='mini'
+                    position='bottom center'
+                    wide />
+                <ul>
+                    <li>
+                        <Link to="/"><i className={`icon home blue small`} style={{ marginRight: '' }}></i>Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact"><i className={`icon envelope orange small`} style={{ scale: '', marginRight: '' }}></i>Contact</Link>
+                    </li>
+                    <li>
+                        <Link to="/projects"><i className={`icon folder open brown small`} style={{ scale: '', marginRight: '' }}></i>Projects</Link>
+                    </li>
+                    <li>
+                        <Link to="/resume"><i className={`icon download green small`} style={{ scale: '', marginRight: '' }}></i>Resume </Link>
+                        {/* <Popup
                         trigger={
                         <Link to="/resume"><i className={`icon download green small`} style={{ scale: '', marginRight: '' }}></i>Resume </Link>
                     }
@@ -48,9 +49,10 @@ const Navbar = () => {
                         inverted
                         size='mini'
                     /> */}
-                </li>
-            </ul>
-        </nav>
+                    </li>
+                </ul>
+            </nav>
+        </>
     )
 }
 
