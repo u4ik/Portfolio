fb8854216cf4586b0790e7699604196899bdd564
u4ik <insighteuphoric@gmail.com>
2023-12-05 23:07:48 -0500

    updated resume

---
+++ src/components/Resume_C/Resume_C.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Resume_C() {
+  return (
+    <div>
+      {"test "}
+      <iframe
+        id="iframepdf"
+        src="../../assets/Amit_Mangat_12_2023.pdf"
+      ></iframe>
+      <p></p>
+    </div>
+  );
+}
+
+export default Resume_C;
--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { ConfirmModal } from '../../components'
-import ResId from './Resume_Helper'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { ConfirmModal, Resume_C } from "../../components";
+
+import ResId from "./Resume_Helper";
 
 const Resume = () => {
-    const handleDownload = (option) => {
-        window.open(option === 'download' ? `https://drive.google.com/uc?export=download&id=${ResId}` : `https://drive.google.com/file/d/${ResId}/view`, option === 'download' ? '' : '_blank')
-    }
-    return (
-        <ConfirmModal text={'Would you like to download my resume?'} callback={handleDownload} navigatePath={"/"} />
-    )
-}
+  //   <Resume_C />;
+  const handleDownload = (option) => {
+    window.open(
+      option === "download"
+        ? `https://drive.google.com/uc?export=download&id=${ResId}`
+        : `https://drive.google.com/file/d/${ResId}/view`,
+      option === "download" ? "" : "_blank"
+    );
+  };
+  return (
+    // <ConfirmModal
+    //   text={"Would you like to download my resume?"}
+    //   callback={handleDownload}
+    //   navigatePath={"/"}
+    // />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100dvh",
+      }}
+    >
+      <iframe
+        src="https://drive.google.com/file/d/1BiKGYaSudSZnpkFBrOR4SEsK4pYidhW2/preview"
+        width="100%"
+        height="83%"
+        allow="autoplay"
+      ></iframe>
+    </div>
+  );
+};
 
-export default Resume
+export default Resume;
